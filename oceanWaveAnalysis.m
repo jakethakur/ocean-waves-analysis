920e604d2f3134bdea046c1b9c1c943aa7e71beb
Jake Thakur <34597857+jakethakur@users.noreply.github.com>
2018-12-18 17:03:51 +0000

    Add files via upload

---
+++ oceanWaveAnalysis.m
@@ -0,0 +1,48 @@
+time = VarName1;
+rawDownData = VarName2;
+
+%Integrating raw data into velocity and displacement
+aveRawDownData = movmean(rawDownData,5)-1;
+downVelData = cumtrapz(9.81*aveRawDownData)*0.00005;
+downVelDataDrift = detrend(downVelData);
+
+%Attempt to curve fit all of the data after linear drift has been removed.
+pvalue = 4;
+pcd = polyfit(time, downVelData, pvalue);
+pvd = polyval(pcd, time);
+downVelDataCorrected = downVelData - pvd;
+
+%Raw down accelerometer data without linear drift removed
+figure;
+plot(time, downVelData);
+xlabel('time');
+ylabel('downVelData');
+
+
+% Down velocity data with a linear drift trend removed
+figure;
+plot(time, downVelDataDrift);
+xlabel('time');
+ylabel('downVelDataDrift');
+
+%Down velocity data corrected polyfit-corrected to attain a shape which resembles
+%what we expect from a wave, as well as the polyfitted values
+figure;
+plot(time, downVelDataCorrected);
+xlabel('time');
+ylabel('downVelDataCorrected');
+
+figure;
+plot(time, pvd);
+xlabel('time');
+ylabel('pcd');
+
+
+%Down displacement data shown after finding cumulative trapeziums of the
+%velocity data
+downDispData = cumtrapz(downVelDataCorrected);
+
+figure;
+plot(time, downDispData);
+xlabel('time');
+ylabel('downDispData');

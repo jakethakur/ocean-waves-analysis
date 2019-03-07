1f2e12e107aa84cb2ac3f2f5b6a7f6a9a943c1a9
Jake Thakur <34597857+jakethakur22@users.noreply.github.com>
2019-03-07 08:02:34 +0000

    New ocean data for analysis

--- oceanWaveAnalysis.m
+++ oceanWaveAnalysis.m
@@ -1,79 +1,88 @@
-% first run vertigo raw csv file on Convert_ocean_data.m
-% then import this data from home -> import data -> select the csv file
+% Saving data as CSV:
+% First run raw vertigo file on load and transform data
+% Then run Convert_ocean_data.m to get this saved as a csv
+
+% Loading data from CSV:
+% Home -> import data -> select the csv file
 % -> output type column vectors -> import selection
-% then run this program (which will use the varName1 and varName2 vars)
+% then run this program (which will use the VarName1 and VarName2 vars)
 
-%time = t;
-%rawDownData = accel_ned(:,3);
+time = VarName1;
+rawDownData = VarName2;
 
-time = imudata(:, 1);
-rawDownData = (accel_ned(:, 3) - 1) * 9.81;
+% Alternatively the following code can be run instead to use ocean data
+% variables straight after load and transform data
 
+% time = imudata(:, 1);
+% rawDownData = accel_ned(:, 3);
+
+
+% Convert to accel from Gs
+downAccel = (rawDownData - 1) * 9.81;
+
+% Plot raw down accel
 figure;
-plot(time, rawDownData);
+plot(time, downAccel);
 xlabel('Time (s)');
-ylabel('Down Accel (m/s)');
+ylabel('Raw Down Acceleration (m/s^2)');
 
 cadence = 5;
-%cadence = 500;
 
 % Cut down data to cadence (smaller sampling rate)
 % moving mean
-aveRawDownData = movmean(rawDownData,cadence)-1;
+aveRawDownData = movmean(downAccel,cadence)-1;
 % decimation (alternate method)
 %aveRawDownData = decimate(rawDownData,cadence);
 %time = decimate(time,cadence);
 
-% Integrating raw data into velocity and displacement
+% Integrate raw data into velocity and displacement
 downVelData = cumtrapz(9.81*aveRawDownData)*0.00005;
 
+
+% Plot raw down velocity
 figure;
 plot(time, downVelData);
 xlabel('Time (s)');
-ylabel('Down Vel (m/s)');
+ylabel('Raw Down Velocity (m/s)');
 
+% Since velocity has a linear drift, detrend can be applied to remove the
+% main body of this drift
 downVelData = detrend(downVelData);
 
-% Down velocity data with a linear drift trend removed
+% Plot down velocity data with a linear drift trend removed
 figure;
 plot(time, downVelData);
 xlabel('Time (s)');
 ylabel('Detrended Down Velocity (m/s)');
 
 
-% Attempt to curve fit all of the data after linear drift has been removed.
+% Attempt to curve fit all of the data after linear drift has been removed
 pvalue = 3;
 pcd = polyfit(time, downVelData, pvalue);
 pvd = polyval(pcd, time);
 downVelDataCorrected = downVelData - pvd;
 
-% Raw down accelerometer data without linear drift removed
-figure;
-plot(time, downVelData);
-xlabel('Time (s)');
-ylabel('Down Velocity (m/s)');
-
-% Down velocity data corrected polyfit-corrected to attain a shape which resembles
-% what we expect from a wave, as well as the polyfitted values
+% Down velocity data corrected polyfit-corrected to attain a shape which
+% resembles what we expect from a wave, as well as the polyfitted values
 figure;
 plot(time, downVelDataCorrected);
 xlabel('Time (s)');
 ylabel('Down Velocity Corrected (m/s)');
 
-% polyfit graph
+% Graph of the polyfit that was subtracted
 figure;
 plot(time, pvd);
 xlabel('Time (s)');
 ylabel('pcd');
 
-% Down displacement data shown after finding cumulative trapeziums of the
-% velocity data
+
+% Integrate again to find down displacement data
 downDispData = cumtrapz(downVelDataCorrected);
 
-% final displacement wave graph
+% Plot final displacement wave graph
 figure;
 plot(time, downDispData);
 xlabel('Time (s)');
 ylabel('Down Displacement (m)');
 
-% now perform turning point analysis on the wave...
+% Now perform turning point analysis on the wave...

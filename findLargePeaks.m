e6dd36da1da4664cc0b857f9fdfa990654fff277
samuel-moore17 <51877286+samuel-moore17@users.noreply.github.com>
2019-06-17 20:19:02 +0100

    Update findLargePeaks.m

--- findLargePeaks.m
+++ findLargePeaks.m
@@ -44,5 +44,16 @@ function [largePeakTimes, largePeakYVals] = findLargePeaks(time, yVals, stdToler
     hold on;
     plot(time, yVals);
     scatter(largePeakTimes, largePeakYVals);
+    
+     % means/STD/RM of *every* point
+    meanElevation = mean(yVals); 
+    rmsElevation = rms(yVals); 
+    stdElevation = std(yVals);
+    sigWaveHeight = 4 * stdElevation;
+    
+    disp(['Standard dev. of wave elevation = ',num2str(stdElevation),'.'])
+    disp(['Significant wave height = ',num2str(sigWaveHeight),'.'])
+    disp(['Mean elevation = ',num2str(meanElevation),'.'])
+    disp(['RMS elevation = ',num2str(rmsElevation),'.'])
 end
 
